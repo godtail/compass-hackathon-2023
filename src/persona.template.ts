75d148e59c300e6971bd71957efe9e85159f8b36
Xusheng Yang <easilyjs@gmail.com>
2023-08-15 21:07:59 +0800

    patch: improve persona template

--- src/persona.template.ts
+++ src/persona.template.ts
@@ -1,26 +1,39 @@
 export const generatePersonaTemplate = (listing: any) => `
-  Generate a buyer's persona profile using the listing information provided to you in JSON: ${JSON.stringify(
-    listing,
-  )}.
-  output the persona profile in following JSON format:
+  Give you a listing information:
+  - address: ${listing.city} ${listing.address}.
+  - price: ${listing.displayPrice}.
+  - key details: ${listing.keyDetails
+    .filter((detail: any) => detail.value !== '-')
+    .map((detail: any) => `${detail.key} is ${detail.value}`)
+    .join(',')}.
+  - public records: ${listing.publicRecords
+    .map(
+      (record: any) =>
+        `${record.name}: ${record.fields
+          .map((field: any) => `${field.key} is ${field.value}`)
+          .join(' and ')}`,
+    )
+    .join(',')}.
+  please generate the listing buyer persona profile in following JSON format:
   {
-    "name": "",
-    "sex": "",
-    "age": "",
-    "jobTitle": "",
-    "annualSalary": "",
-    "location": "",
-    "quote": "",
-    "bio": "",
-    "tags": [],
-    "goals": [],
-    "painPoints": [],
-    "decisionCriteria": [],
-    "likes": [],
-    "dislikes": [],
+    "name": string,
+    "sex": string,
+    "age": string,
+    "jobTitle": string,
+    "annualSalary": string,
+    "location": string,
+    "quote": string,
+    "bio": string,
+    "tags": string[],
+    "goals": string[],
+    "painPoints": string[],
+    "decisionCriteria": string[],
+    "likes": string[],
+    "dislikes": string[],
   }
-  Rules:
+  profile rules:
   - sex available values: male, female.
   - age available values: adult, young-adult, child, infant, elderly.
   - name and sex should match.
+  - generate jobTitle & annualSalary.
 `;
